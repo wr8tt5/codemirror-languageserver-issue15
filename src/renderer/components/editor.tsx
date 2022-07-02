5eb2d051a50f3b2d74c7384e1013525ad2632d91
Wayne Ellison <wr8tt5@gmail.com>
2022-07-02 16:12:40 +1000

    Still reproducible without codemirror-languageserver

--- src/renderer/components/editor.tsx
+++ src/renderer/components/editor.tsx
@@ -1,19 +1,6 @@
 import CodeMirror from '@uiw/react-codemirror'
-import { Facet, Extension } from '@codemirror/state'
 import { javascript } from '@codemirror/lang-javascript'
-import { languageServerWithTransport } from 'codemirror-languageserver'
 import React from 'react'
-import { DummyTransport } from '../dummy_transport'
-
-const lsOptions = {
-    transport: new DummyTransport(),
-    rootUri: 'file:///',
-    documentUri: 'file:///myfile.ts',
-    languageId: 'typescript',
-    workspaceFolders: []
-}
-
-var ls: Extension[] = languageServerWithTransport({...lsOptions})
 
 export const Editor = React.memo(function Editor() {
 
@@ -21,7 +8,6 @@ export const Editor = React.memo(function Editor() {
         <CodeMirror
             extensions={[
                 javascript({ typescript: true }),
-                ls
             ]}
             height="200px"
         />
